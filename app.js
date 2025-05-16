e739a27ecdce6373d2f00036c72a41c045a7b17f
dannyarena <arenadanny71@gmail.com>
2025-05-16 16:07:06 +0200

    add error handling middleware and not found middleware files

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require ('express');
 const app = express();
-app.use(express.json());
+app.use(express.json());  // Middleware per il parsing del JSON
 const postsRouter = require('./routers/posts');
 
 app.use('/posts', postsRouter);
