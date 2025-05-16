e52dfb010f7022728852d6c0da61f2ab5f9f6c8b
dannyarena <arenadanny71@gmail.com>
2025-05-16 16:24:05 +0200

    add error handling middleware to manage internal server errors

---
+++ middlewares/errorsHandler.js
@@ -0,0 +1,8 @@
+function errorsHandler(err, req, res, next) {
+    console.error(err.stack);
+
+    res.status(500).json({
+        error: 'Internal Server Error',
+        message: 'Qualcosa è andato storto'
+    })
+}
